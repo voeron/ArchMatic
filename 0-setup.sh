127cec4e71f1af4c3522f9adc844d9d9c1f94b05
voeron <olsio@hotmail.com>
2021-01-12 21:54:44 +0200

    removed mirror update

--- 0-setup.sh
+++ 0-setup.sh
@@ -30,8 +30,8 @@ echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download - US Only"
 echo "-------------------------------------------------"
 pacman -S --noconfirm pacman-contrib curl
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=FI&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+#mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+#curl -s "https://www.archlinux.org/mirrorlist/?country=FI&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 nc=$(grep -c ^processor /proc/cpuinfo)
 echo "You have " $nc" cores."
